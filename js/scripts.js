03a80558e3d9fdebe3b492affda478eb872cd861
Sergio B <sbarajas510@gmail.com>
2020-02-09 23:31:11 -0800

    getting full info from 12  employee

--- js/scripts.js
+++ js/scripts.js
@@ -7,17 +7,21 @@ fetch('https://randomuser.me/api/?results=12&inc=name,location,email,picture&nat
 
     
 function generateCard(data){
-    employee = data.results[0]
-    const html= `<div class="card">
+    let employee = data.results;
+    //console.log(employee)
+
+    const html= employee.map( person => {
+
+    return `<div class="card">
     <div class="card-img-container">
-        <img class="card-img" src="${employee.picture.large}" alt="profile picture">
+        <img class="card-img" src="${person.picture.large}" alt="profile picture">
     </div>
     <div class="card-info-container">
-        <h3 id="name" class="card-name cap">${employee.name.last}, ${employee.name.first}</h3>
-        <p class="card-text">${employee.email}</p>
-        <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
+        <h3 id="name" class="card-name cap">${person.name.last}, ${person.name.first}</h3>
+        <p class="card-text">${person.email}</p>
+        <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
     </div>
-</div>`;
+</div>`}).join('');
 gallery.innerHTML = html;
-console.log(data.results)
+//console.log(data.results)
 }
